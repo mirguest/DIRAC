e1667be7686a70c35bb9eaab840b1ab6f482ce10
lint <lintao@ihep.ac.cn>
2013-01-13 10:13:34 +0800

    Add more rpc.

--- DataManagementSystem/scripts/bes-dirac-dms-simple-message.py
+++ DataManagementSystem/scripts/bes-dirac-dms-simple-message.py
@@ -5,11 +5,16 @@ Script.parseCommandLine(ignoreErrors=False)
 from DIRAC.Core.DISET.RPCClient import RPCClient
 
 simpleMessageService = RPCClient('dips://besdirac02.ihep.ac.cn:62222/DataManagement/SimpleMessage')
+#simpleMessageService = RPCClient('dips://besdirac02.ihep.ac.cn:62222/Framework/SimpleMessage')
+#simpleMessageService = RPCClient('dips://besdirac02.ihep.ac.cn:42222/Framework/SimpleMessage')
+#simpleMessageService = RPCClient('dips://besdirac02.ihep.ac.cn:32222/Accounting/SimpleMessage')
 
 result = simpleMessageService.getMessage()
+
+print result
 
 if not result['OK']:
   print "Error while calling the service:", result['Message']
 else:
-  for k, v in result['Values'].items():
+  for k, v in result['Value'].items():
     print k, v
